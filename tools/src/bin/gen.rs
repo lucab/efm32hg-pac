48a1114d86addf52a0bcd04019bc8e8fb753b4fe
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-11-26 15:10:49 -0700

    gen: aesthetic

--- tools/src/bin/gen.rs
+++ tools/src/bin/gen.rs
@@ -1,16 +1,14 @@
-extern crate form;
-use form::create_directory_structure;
+use std::fs::{read_to_string, File};
 use std::io::Write;
-use svd2rust::util::build_rs;
-
-use std::fs::File;
-
-use std::io::Read;
-
-extern crate svd2rust;
-use svd2rust::{generate, target::Target, util::SvdError};
+use std::path::PathBuf;
 
 extern crate svd_parser as svd;
+
+extern crate svd2rust;
+use svd2rust::{generate, target::Target, util::build_rs, util::SvdError};
+
+extern crate form;
+use form::create_directory_structure;
 
 extern crate quote;
 use quote::{multi_zip_expr, nested_tuples_pat, pounded_var_names, quote, quote_each_token};
@@ -18,28 +16,22 @@ use quote::{multi_zip_expr, nested_tuples_pat, pounded_var_names, quote, quote_e
 extern crate rustfmt_nightly;
 use rustfmt_nightly::{Config, Input, Session};
 
-use std::path::PathBuf;
-
 pub fn main() -> Result<(), SvdError> {
-    let name = "svd/EFM32HG309F64.svd";
-    let output_dir = "src/";
-
-    //read svd
-    let xml = &mut String::new();
-    File::open(name)?.read_to_string(xml)?;
-    let device = svd::parse(xml);
+    //parse xml
+    let xml = read_to_string("svd/EFM32HG309F64.svd")?;
+    let device = svd::parse(&xml);
 
     //parse svd
     let mut device_x = String::new();
     let items = generate::device::render(&device, &Target::CortexM, true, &mut device_x)?;
 
-    //save
-    writeln!(File::create("device.x").unwrap(), "{}", device_x).unwrap();
-    writeln!(File::create("build.rs").unwrap(), "{}", build_rs()).unwrap();
+    //save other files
+    writeln!(File::create("device.x")?, "{}", device_x)?;
+    writeln!(File::create("build.rs")?, "{}", build_rs())?;
 
-    //form lib and save
-    let input = format!("{}", quote!(#(#items)*));
-    create_directory_structure(output_dir, input)?;
+    //form lib.rs and save
+    let lib = format!("{}", quote!(#(#items)*));
+    create_directory_structure("src/", lib)?;
 
     //rustfmt saved files
     let files = ["build.rs", "src/lib.rs"].iter().map(|a| PathBuf::from(a));
