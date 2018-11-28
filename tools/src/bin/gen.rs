ffc84a939c361929932fa260e31a00163e7e1e7c
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-11-27 20:30:47 -0700

    gen: use my fork of tones111 svd2rust

--- tools/src/bin/gen.rs
+++ tools/src/bin/gen.rs
@@ -3,23 +3,26 @@ use rustfmt_nightly::{Config, Input, Session};
 use std::fs::{read_to_string, File};
 use std::io::Write;
 use std::path::PathBuf;
-use svd2rust::{generate, Generation, Target};
-use svd_parser;
+use svd2rust::{generate, Generation, Target::CortexM};
 
 pub fn main() -> () {
     //parse xml
     let xml = read_to_string("svd/EFM32HG309F64.svd").unwrap();
-    let Generation { lib_rs, device_x } = generate(&xml, &Target::CortexM, true).unwrap();
+    let Generation {
+        lib_rs,
+        device_x,
+        build_rs,
+    } = generate(&xml, &CortexM, true).unwrap();
 
     //save other files
     writeln!(File::create("device.x").unwrap(), "{}", device_x).unwrap();
-    // writeln!(File::create("build.rs").unwrap(), "{}", build_rs()).unwrap();
+    writeln!(File::create("build.rs").unwrap(), "{}", build_rs).unwrap();
 
     //form lib.rs and save
     create_directory_structure("src/", lib_rs).unwrap();
 
     //rustfmt saved files
-    let files = ["src/lib.rs"].iter().map(|a| PathBuf::from(a));
+    let files = ["build.rs", "src/lib.rs"].iter().map(|a| PathBuf::from(a));
     let mut buf = Vec::<u8>::new();
     let mut session = Session::new(Config::default(), Some(&mut buf));
 
